aeb317b0e0410899dafd7cc388f6a2c42116d845
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-02-12 14:54:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "37969182"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "8aca60eb9495680a94583c1b28738379"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = int(getenv("OWNER_ID", "7538572906"))
+SUPPORT_GRP = "Yaaro_kimehfill"
+UPDATE_CHNL = "BotzEmpire"
+OWNER_USERNAME = "aboutdeva"
     
