f49d26805bfee607ac7bda8074af0f49a9f72f63
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-02-13 12:26:58 +0530

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -1,21 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from nexichat import nexichat
 
-"""@nexichat.on_message(filters.incoming)
+# yahan apna app import karo
+# agar main file ka naam main.py hai:
+from main import app
+# agar main file ka naam bot.py hai, to upar wali line ko badal ke:
+# from bot import app
+
+@app.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
         await message.react("👍")
     except Exception as e:
-        print(f"Failed to react to message: {e}")"""
-
-from telethon import TelegramClient, events
-
-
-@nexichat.on(events.NewMessage(incoming=True))
-async def react_to_messages(event):
-    try:
-        await event.message.respond("👍")  # Telethon में `send_reaction` अभी Stable Version में नहीं है
-    except Exception as e:
         print(f"Failed to react to message: {e}")
-
